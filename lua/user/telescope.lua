77d006a409ab2d9e7d633aeb78c013a17768d22f
Halil Bilir <hbilir@gmail.com>
2024-10-03 12:58:39 +0300

    Fix trouble exception.

--- lua/user/telescope.lua
+++ lua/user/telescope.lua
@@ -1,118 +1,123 @@
 local status_ok, telescope = pcall(require, "telescope")
 if not status_ok then
-return
+	return
 end
 
 local M = {}
 
-local actions = require "telescope.actions"
-local trouble = require("trouble.providers.telescope")
+local actions = require("telescope.actions")
+local trouble = require("trouble.sources.telescope")
 
-telescope.setup {
-  defaults = {
-    prompt_prefix = " ",
-    selection_caret = " ",
-    path_display = { "truncate" },
+telescope.setup({
+	defaults = {
+		prompt_prefix = " ",
+		selection_caret = " ",
+		path_display = { "truncate" },
 
-    mappings = {
-      i = {
-        ["<c-t>"] = trouble.open_with_trouble,
-        ["<C-n>"] = actions.cycle_history_next,
-        ["<C-p>"] = actions.cycle_history_prev,
-        ["<C-j>"] = actions.move_selection_next,
-        ["<C-k>"] = actions.move_selection_previous,
-        ["<C-c>"] = actions.close,
-        ["<Down>"] = actions.move_selection_next,
-        ["<Up>"] = actions.move_selection_previous,
-        ["<CR>"] = actions.select_default,
-        ["<C-x>"] = actions.select_horizontal,
-        ["<C-v>"] = actions.select_vertical,
-        ["<C-u>"] = actions.preview_scrolling_up,
-        ["<C-d>"] = actions.preview_scrolling_down,
-        ["<PageUp>"] = actions.results_scrolling_up,
-        ["<PageDown>"] = actions.results_scrolling_down,
-        ["<Tab>"] = actions.toggle_selection + actions.move_selection_worse,
-        ["<S-Tab>"] = actions.toggle_selection + actions.move_selection_better,
-        ["<C-q>"] = actions.send_to_qflist + actions.open_qflist,
-        ["<M-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
-        ["<C-l>"] = actions.complete_tag,
-        ["<C-_>"] = actions.which_key, -- keys from pressing <C-/>
-      },
-      n = {
-        ["<c-t>"] = trouble.open_with_trouble,
-        ["<C-c>"] = actions.close,
-        ["<CR>"] = actions.select_default,
-        ["<C-x>"] = actions.select_horizontal,
-        ["<C-v>"] = actions.select_vertical,
-        ["<Tab>"] = actions.toggle_selection + actions.move_selection_worse,
-        ["<S-Tab>"] = actions.toggle_selection + actions.move_selection_better,
-        ["<C-q>"] = actions.send_to_qflist + actions.open_qflist,
-        ["<M-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
-        ["j"] = actions.move_selection_next,
-        ["k"] = actions.move_selection_previous,
-        ["H"] = actions.move_to_top,
-        ["M"] = actions.move_to_middle,
-        ["L"] = actions.move_to_bottom,
-        ["<Down>"] = actions.move_selection_next,
-        ["<Up>"] = actions.move_selection_previous,
-        ["gg"] = actions.move_to_top,
-        ["G"] = actions.move_to_bottom,
-        ["<C-u>"] = actions.preview_scrolling_up,
-        ["<C-d>"] = actions.preview_scrolling_down,
-        ["<PageUp>"] = actions.results_scrolling_up,
-        ["<PageDown>"] = actions.results_scrolling_down,
-        ["?"] = actions.which_key,
-      },
-    },
-  },
-}
+		mappings = {
+			i = {
+				["<c-t>"] = trouble.open,
+				["<C-n>"] = actions.cycle_history_next,
+				["<C-p>"] = actions.cycle_history_prev,
+				["<C-j>"] = actions.move_selection_next,
+				["<C-k>"] = actions.move_selection_previous,
+				["<C-c>"] = actions.close,
+				["<Down>"] = actions.move_selection_next,
+				["<Up>"] = actions.move_selection_previous,
+				["<CR>"] = actions.select_default,
+				["<C-x>"] = actions.select_horizontal,
+				["<C-v>"] = actions.select_vertical,
+				["<C-u>"] = actions.preview_scrolling_up,
+				["<C-d>"] = actions.preview_scrolling_down,
+				["<PageUp>"] = actions.results_scrolling_up,
+				["<PageDown>"] = actions.results_scrolling_down,
+				["<Tab>"] = actions.toggle_selection + actions.move_selection_worse,
+				["<S-Tab>"] = actions.toggle_selection + actions.move_selection_better,
+				["<C-q>"] = actions.send_to_qflist + actions.open_qflist,
+				["<M-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
+				["<C-l>"] = actions.complete_tag,
+				["<C-_>"] = actions.which_key, -- keys from pressing <C-/>
+			},
+			n = {
+				["<c-t>"] = trouble.open,
+				["<C-c>"] = actions.close,
+				["<CR>"] = actions.select_default,
+				["<C-x>"] = actions.select_horizontal,
+				["<C-v>"] = actions.select_vertical,
+				["<Tab>"] = actions.toggle_selection + actions.move_selection_worse,
+				["<S-Tab>"] = actions.toggle_selection + actions.move_selection_better,
+				["<C-q>"] = actions.send_to_qflist + actions.open_qflist,
+				["<M-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
+				["j"] = actions.move_selection_next,
+				["k"] = actions.move_selection_previous,
+				["H"] = actions.move_to_top,
+				["M"] = actions.move_to_middle,
+				["L"] = actions.move_to_bottom,
+				["<Down>"] = actions.move_selection_next,
+				["<Up>"] = actions.move_selection_previous,
+				["gg"] = actions.move_to_top,
+				["G"] = actions.move_to_bottom,
+				["<C-u>"] = actions.preview_scrolling_up,
+				["<C-d>"] = actions.preview_scrolling_down,
+				["<PageUp>"] = actions.results_scrolling_up,
+				["<PageDown>"] = actions.results_scrolling_down,
+				["?"] = actions.which_key,
+			},
+		},
+	},
+})
 
 function M.reload()
--- Telescope will give us something like user/colors.lua,
-  -- so this function convert the selected entry to
-  -- the module name: user.colors
-  local function get_module_name(s)
-    local module_name;
+	-- Telescope will give us something like user/colors.lua,
+	-- so this function convert the selected entry to
+	-- the module name: user.colors
+	local function get_module_name(s)
+		local module_name
 
-    module_name = s:gsub("%.lua", "")
-    module_name = module_name:gsub("%/", ".")
-    module_name = module_name:gsub("%.init", "")
+		module_name = s:gsub("%.lua", "")
+		module_name = module_name:gsub("%/", ".")
+		module_name = module_name:gsub("%.init", "")
 
-    return module_name
-  end
+		return module_name
+	end
 
-  local prompt_title = "~ neovim modules ~"
+	local prompt_title = "~ neovim modules ~"
 
-  -- sets the path to the lua folder
-  local path = "~/.config/nvim/lua"
+	-- sets the path to the lua folder
+	local path = "~/.config/nvim/lua"
 
-  local opts = {
-    prompt_title = prompt_title,
-    cwd = path,
+	local opts = {
+		prompt_title = prompt_title,
+		cwd = path,
 
-    attach_mappings = function(_, map)
-     -- Adds a new map to ctrl+r.
-      map("i", "<c-r>", function(_)
-        -- these two a very self-explanatory
-        local entry = require("telescope.actions.state").get_selected_entry()
-        local name = get_module_name(entry.value)
+		attach_mappings = function(_, map)
+			-- Adds a new map to ctrl+r.
+			map("i", "<c-r>", function(_)
+				-- these two a very self-explanatory
+				local entry = require("telescope.actions.state").get_selected_entry()
+				local name = get_module_name(entry.value)
 
-        -- call the helper method to reload the module
-        -- and give some feedback
-        R(name)
-        P(name .. " RELOADED!!!")
-      end)
+				-- call the helper method to reload the module
+				-- and give some feedback
+				R(name)
+				P(name .. " RELOADED!!!")
+			end)
 
-      return true
-    end
-  }
+			return true
+		end,
+	}
 
-  -- call the builtin method to list files
-  require('telescope.builtin').find_files(opts)
+	-- call the builtin method to list files
+	require("telescope.builtin").find_files(opts)
 end
 
 local api = vim.api
 api.nvim_set_keymap("n", ",", "<cmd>Telescope find_files theme=get_dropdown<CR>", { noremap = true, silent = true })
-api.nvim_set_keymap("n", "ü", "<cmd>Telescope buffers theme=get_dropdown sort_lastused=true<CR>", { noremap = true, silent = true })
+api.nvim_set_keymap(
+	"n",
+	"ü",
+	"<cmd>Telescope buffers theme=get_dropdown sort_lastused=true<CR>",
+	{ noremap = true, silent = true }
+)
 
 return M
